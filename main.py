0933c85cc593a6e06ad9f141381fddbdadf62808
Cade Lanktree <lanktree.cade@gmail.com>
2021-02-06 17:13:54 -0500

    restarting; found a better solution
    selenium wasn't working as intended

--- main.py
+++ main.py
@@ -1,33 +1,4 @@
 #import modules 
-from selenium import webdriver 
-from selenium.webdriver.common.keys import Keys 
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.support.ui import WebDriverWait 
-from selenium.webdriver.support import expected_conditions as EC 
-#initialize webdriver 
-PATH = "/Applications/Firefox.app" 
-driver = webdriver.Firefox(PATH)
-#navigate to web page 
-driver.get("https://www.reddit.com/") 
-#locate search box 
-search = driver.find_element_by_name("q") 
-#enter search term 
-search.send_keys("scraping") 
-search.send_keys(Keys.RETURN) 
-
-try: 
-#locate search results 
-    search_results = WebDriverWait(driver, 20).until( 
-    EC.presence_of_element_located((By.CLASS_NAME, "rpBJOHq2PR60pnwJlUyP0")) 
-    ) 
-#scrape posts' headings 
-    posts = search_results.find_elements_by_css_selector("h3._eYtD2XCVieq6emjKBH3m") 
-    for post in posts: 
-        header = post.find_element_by_tag_name("span") 
-        print(header.text) 
-finally: 
-#quit browser 
-    driver.quit()
 
 
 
